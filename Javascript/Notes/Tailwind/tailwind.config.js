f11a3f0b4c550a57258640d9668ed185fba3445f
Nilusche Liyanaarachchi <Niluschel@outlook.de>
2022-02-25 02:07:07 +0100

    Tailwind excourse

---
+++ Javascript/Notes/Tailwind/tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      colors:{
+        primary:'#ff6363',
+        secondary: {
+          100:"#e2e2d5",
+          200:"#888883"
+        }
+
+      },
+      fontFamilty:{
+        body:['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
