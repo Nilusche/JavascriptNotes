ba8eda0bf17641f1668c7c1d56a6e51ee541c9c0
Nilusche Liyanaarachchi <Niluschel@outlook.de>
2022-05-10 17:48:03 +0200

    planner

---
+++ VueJs/Notes/VueCLI/planner/src/components/SingleProject.vue
@@ -0,0 +1,76 @@
+<template>
+    <div class="project" :class="{complete: project.complete}">
+        <div class="actions">
+            <h3  @click="details = !details">{{project.title}}</h3>
+            <div class="icons">
+                <span class="material-symbols-outlined">edit</span>
+                <span class="material-symbols-outlined" @click="deleteProject">delete</span>
+                <span class="material-symbols-outlined tick" @click="completeProject">done</span>
+            </div>
+        </div>
+        <div v-if="details" class="details">
+            <p>{{project.details}}</p>
+        
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props:["project"],
+    data(){
+        return{
+            details: false,
+            uri: "http://localhost:3000/projects/" + this.project.id
+        }
+    },
+    methods:{
+        deleteProject(){
+            fetch(this.uri, {method:'DELETE'})
+            .then(()=> this.$emit('delete', this.project.id))
+            .catch((e)=>console.log(e))
+        },
+        completeProject(){
+            fetch(this.uri, {method: "PATCH", headers:{'Content-Type':'application/json'}, body: JSON.stringify({complete: !this.project.complete})})
+            .then(()=>{
+                this.$emit("complete", this.project.id)
+            })
+        }
+
+    }
+}
+</script>
+
+<style>
+.project{
+    margin:20px auto;
+    background: white;
+    padding: 10px 20px;
+    border-radius: 4px;
+    box-shadow: 1px 3px rgba(0,0,0,0,0.05);
+    border-left: 4px solid #e90074;
+}
+h3{
+    cursor:pointer;
+}
+.actions{
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
+}
+.material-symbols-outlined{
+    font-size:24px;
+    margin-left:10px;
+    color:#bbb;
+    cursor:pointer;
+}
+.material-symbols-outlined:hover{
+    color:#777;
+}
+.complete{
+    border-left: 4px solid #00ce89;
+}
+.complete .tick{
+    color : #00ce89;
+}
+</style>
