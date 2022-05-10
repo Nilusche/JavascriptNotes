69ed455bdb542ae6b25ad6af560c38d2551abb66
Nilusche Liyanaarachchi <Niluschel@outlook.de>
2022-05-10 16:16:11 +0200

    Vuerouter introduction

---
+++ VueJs/Notes/VueCLI/Vuerouter/src/views/HomeView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est, voluptates adipisci eveniet hic sit pariatur ea voluptas rerum repellat, quam, expedita unde itaque magnam consectetur nesciunt voluptatum ullam officia fugit nobis animi possimus. Nam, aperiam. Adipisci, eveniet. Sit iusto nemo voluptas ab similique voluptatum iure, aliquid reiciendis unde assumenda autem quisquam suscipit atque excepturi, harum praesentium minima odio illo nesciunt repellendus veritatis! Beatae deserunt expedita nesciunt sapiente, dolores quisquam quis repellendus facere voluptate ut cumque qui commodi unde temporibus pariatur libero ex dolorum animi distinctio? Esse illum reprehenderit reiciendis facilis eos id consequatur quo officiis earum provident maxime, eveniet corporis.</p>
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Est, voluptates adipisci eveniet hic sit pariatur ea voluptas rerum repellat, quam, expedita unde itaque magnam consectetur nesciunt voluptatum ullam officia fugit nobis animi possimus. Nam, aperiam. Adipisci, eveniet. Sit iusto nemo voluptas ab similique voluptatum iure, aliquid reiciendis unde assumenda autem quisquam suscipit atque excepturi, harum praesentium minima odio illo nesciunt repellendus veritatis! Beatae deserunt expedita nesciunt sapiente, dolores quisquam quis repellendus facere voluptate ut cumque qui commodi unde temporibus pariatur libero ex dolorum animi distinctio? Esse illum reprehenderit reiciendis facilis eos id consequatur quo officiis earum provident maxime, eveniet corporis.</p>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+
+export default {
+  name: 'HomeView',
+  components: {
+    
+  }
+}
+</script>
